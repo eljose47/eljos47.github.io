8f8632e4b074d81f4a36da99275999f252005d10
Josef Hendrikson <jh@intelligentgraphics.biz>
2023-01-08 19:14:50 +0100

    add styled-components; styling; add header with twitch app

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
+import styled from "styled-components";
+import Header from "./header";
+
+const Content = styled.div`
+  padding: 8px;
+`;
 
 const App: React.FunctionComponent<AppProps> = (props) => {
   return (
-    <div>
-      <h1>huhu</h1>
-    </div>
+    <>
+      <Header></Header>
+      <Content>Work In Progress</Content>
+    </>
   );
 };
 
